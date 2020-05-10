131d8eab2717d3b8a0c14a8b8b1c6af6af1ae4a0
Brendan Gallagher <bpgallagher27@gmail.com>
2020-05-10 08:26:04 -0400

    rendered employees

--- emp-dir/src/App.js
+++ emp-dir/src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Employee from "./components/Employee";
+import employees from "./employees.json";
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {
+    employees
+  };
+
+  render() {
+    return (
     <div className="column jumbotron">
       <Wrapper>
         <Title><a href="https://www.imdb.com/title/tt1375666/?ref_=nv_sr_srsg_0">"Inception"</a> Employee Directory</Title>
+        {this.state.employees.map(employees => (
+          <Employee
+            removeFriend={this.removeFriend}
+            id={employees.id}
+            key={employees.id}
+            name={employees.name}
+            image={employees.image}
+            role={employees.role}
+            salary={employees.salary}
+          />
+        ))}
       </Wrapper>
     </div>
-  );
-}
+  )};
+};
 
 export default App;
--- emp-dir/src/components/Employee/index.js
+++ emp-dir/src/components/Employee/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Employee(props) {
     return (
-      <div className="card">
+      <div className="card row">
         <div className="img-container">
           <img alt={props.name} src={props.image} />
         </div>
