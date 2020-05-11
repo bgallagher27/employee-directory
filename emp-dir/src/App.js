f89a293eaea71c44b649cc4deb67358ae6c6a544
Brendan Gallagher <bpgallagher27@gmail.com>
2020-05-11 17:45:28 -0400

    created director filter

--- emp-dir/src/App.js
+++ emp-dir/src/App.js
@@ -2,29 +2,31 @@ import React from 'react';
 import './App.css';
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import Employee from "./components/Employee";
-import employees from "./employees.json";
+// import Employee from "./components/Employee";
+// import employees from "./employees.json";
 
 class App extends React.Component {
-  state = {
-    employees
-  };
+  // state = {
+  //   employees
+  // };
+
+  // showEmployees = () => {
+  //   this.state.employees.map(employees => (
+  //     <Employee
+  //       id={employees.id}
+  //       key={employees.id}
+  //       name={employees.name}
+  //       image={employees.image}
+  //       role={employees.role}
+  //       salary={employees.salary}
+  //     />
+  //   ))}
 
   render() {
     return (
     <div className="jumbotron">
       <Wrapper>
         <Title><a href="https://www.imdb.com/title/tt1375666/?ref_=nv_sr_srsg_0">"Inception"</a> Employee Directory</Title>
-        {this.state.employees.map(employees => (
-          <Employee
-            id={employees.id}
-            key={employees.id}
-            name={employees.name}
-            image={employees.image}
-            role={employees.role}
-            salary={employees.salary}
-          />
-        ))}
       </Wrapper>
     </div>
   )};
--- emp-dir/src/components/Title/index.js
+++ emp-dir/src/components/Title/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import Sort from "../Sort";
 import Filter from "../Filter";
 import "./style.css";
+import employees from "../../employees.json";
+import Employee from "../Employee";
 
-function Title(props) {
+class Title extends React.Component {
+
+    state = {
+        employees
+    };
+
+    filterDirector = () => {
+        const employees = this.state.employees.filter(employees => employees.role === "Director");
+    this.setState({ employees });
+    };
+
+    render() {
     return (
-        <div className>
+        <div>
             <div className="row">
-                <h1 className="title col jumbotron">{ props.children }</h1>
+                <h1 className="title col jumbotron">{ this.props.children }</h1>
             </div>
             <div className="row">
                 <div className="sort col">
@@ -16,11 +29,22 @@ function Title(props) {
             </div>
             <div className="row">
                 <div className="filter col">
-                    <Filter />
+                    <Filter filterDirector={this.filterDirector} />
                 </div>
             </div>
+            {this.state.employees.map(employees => (
+                <Employee
+                    id={employees.id}
+                    key={employees.id}
+                    name={employees.name}
+                    image={employees.image}
+                    role={employees.role}
+                    salary={employees.salary}
+                />
+            ))}
         </div>
     )
+    }
 };
 
 export default Title;
